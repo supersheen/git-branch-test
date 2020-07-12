445321ce98678e7c81999bc495a9c3d879f037ab
lichaoshen <lichao.shen@adthena.com>
2020-07-13 00:17:50 +0100

    base-one-feature-two change add number class, add 3 numbers.

--- my-app/src/main/java/com/mycompany/utils/AddNumbers.java
+++ my-app/src/main/java/com/mycompany/utils/AddNumbers.java
@@ -5,4 +5,8 @@ public class AddNumbers {
     public int addTwoNumbers(int a, int b) {
         return a + b;
     }
+
+    public int minusThreeNumbers(int a, int b, int c) {
+        return a + b + c;
+    }
 }
