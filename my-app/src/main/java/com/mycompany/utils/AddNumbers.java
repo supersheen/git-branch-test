78e589ce1c37f67f6a50db70960dab9ff78f48eb
lichaoshen <lichao.shen@adthena.com>
2020-07-12 23:28:55 +0100

    epic branch

--- my-app/src/main/java/com/mycompany/utils/AddNumbers.java
+++ my-app/src/main/java/com/mycompany/utils/AddNumbers.java
@@ -2,7 +2,7 @@ package com.mycompany.utils;
 
 public class AddNumbers {
 
-    int addTwoNumbers(int a, int b) {
+    public int addTwoNumbers(int a, int b) {
         return a + b;
     }
 }
---
+++ my-app/src/main/java/com/mycompany/utils/PositiveNegative.java
@@ -0,0 +1,8 @@
+package com.mycompany.utils;
+
+public class PositiveNegative {
+
+    public boolean isPositive(int a) {
+        return a > 0;
+    }
+}
