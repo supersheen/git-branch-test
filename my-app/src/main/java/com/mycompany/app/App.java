9a72388d2d080b2710fdea0a71578c53b2de9b3e
lichaoshen <lichao.shen@adthena.com>
2020-07-13 01:00:44 +0100

    added by feature two branch

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World this is feature one branch! keep changing main class after import." );
+        System.out.println( "Hello World this is feature one branch! keep changing main class after import. for feature four branch. " );
     }
 }
