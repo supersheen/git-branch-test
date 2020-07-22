f221ba64703dcad77370a330c956ef4d09bc5c71
lichaoshen <lichao.shen@adthena.com>
2020-07-22 10:42:18 +0100

    Revert "Revert "Merge pull request #7 from supersheen/base-one-feature-one-fix""
    This reverts commit 624e1f22

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World this is feature one branch! FIX" );
+        System.out.println( "New Feature Work containing error" );
         System.out.println( "more work on base branch by other devs" );
     }
 }
