ca1e0db75fb609951de100e5a18b25ccabb1db1e
lichaoshen <lichao.shen@adthena.com>
2020-07-22 10:36:49 +0100

    fix for branch five

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World this is feature one branch! FIX" );
-        System.out.println( "New Feature Work containing error" );
+        System.out.println( "New Feature Work with fixed error" );
     }
 }
