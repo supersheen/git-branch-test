48f1544c6165027e3d9f79a97f5ff040c1254000
lichaoshen <lichao.shen@adthena.com>
2020-07-28 00:18:28 +0100

    fix for the broken and reverted feature branch

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "branch with error !" );
+        System.out.println( "branch with error ! but I fixed it now in a new branch." );
     }
 }
