73f23b4d3dea39006fd95d595743dfa8edbf593e
lichaoshen <lichao.shen@adthena.com>
2020-07-29 23:18:35 +0100

    work from other people

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -10,4 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public static void anotherMethod() {
+        System.out.println( "work from other people" );
+    }
 }
